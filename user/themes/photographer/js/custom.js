2d72600454cbf4290e7f9b61cd33ed1fd2d30c3d
Yuri Cabrera <yuri881029@gmail.com>
2021-11-09 13:05:35 +0300

    fixing vertical slide

--- user/themes/photographer/js/custom.js
+++ user/themes/photographer/js/custom.js
@@ -84,7 +84,9 @@ $('.vertical-slide').slick({
   adaptiveHeight: true,
   dots: true,
 }).slickLightbox({
-  useHistoryApi: 'true'
+  useHistoryApi: 'true',
+  src: 'src',
+  itemSelector: 'figure img'
 });
 $('.vertical-slide').on("beforeChange", function(event, slick) {
     slick = $(slick.$slider);
